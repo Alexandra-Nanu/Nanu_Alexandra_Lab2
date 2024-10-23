62b8b393d2da7602863ff84226887cb685b79493
alexa <alexa@ALEXANDRA_NANU>
2024-10-23 19:17:07 +0300

    Sarcina de laborator

---
+++ Models/Author.cs
@@ -0,0 +1,21 @@
+﻿namespace Nanu_Alexandra_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        public ICollection<Book>? Books { get; set; }
+
+
+    }
+}
--- Pages/Authors/Index.cshtml.cs
+++ Pages/Authors/Index.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Nanu_Alexandra_Lab2.Data;
 using Nanu_Alexandra_Lab2.Models;
 
-namespace Nanu_Alexandra_Lab2.Pages.Books
+namespace Nanu_Alexandra_Lab2.Pages.Authors
 {
     public class IndexModel : PageModel
     {
@@ -19,14 +19,11 @@ namespace Nanu_Alexandra_Lab2.Pages.Books
             _context = context;
         }
 
-        public IList<Book> Book { get;set; } = default!;
+        public IList<Author> Author { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
-            Book = await _context.Book
-                .Include(b=>b.Publisher)
-                .Include(b => b.Author)
-                .ToListAsync();
+            Author = await _context.Author.ToListAsync();
         }
     }
 }
