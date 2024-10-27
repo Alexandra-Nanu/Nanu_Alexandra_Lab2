cdf0ee6fd409b5406a26ec5c98b92d4f021e8ac7
alexa <alexa@ALEXANDRA_NANU>
2024-10-27 14:38:44 +0200

    Laborator 3

--- Pages/Categories/Details.cshtml.cs
+++ Pages/Categories/Details.cshtml.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Nanu_Alexandra_Lab2.Data;
 using Nanu_Alexandra_Lab2.Models;
 
-namespace Nanu_Alexandra_Lab2.Pages.Books
+namespace Nanu_Alexandra_Lab2.Pages.Categories
 {
     public class DetailsModel : PageModel
     {
@@ -20,7 +19,7 @@ namespace Nanu_Alexandra_Lab2.Pages.Books
             _context = context;
         }
 
-        public Book Book { get; set; } = default!;
+        public Category Category { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -28,21 +27,15 @@ namespace Nanu_Alexandra_Lab2.Pages.Books
             {
                 return NotFound();
             }
-            Book = await _context.Book
-                .Include(b => b.Publisher)
-                .Include(b => b.Author)
-                .Include(b => b.BookCategories)
-                    .ThenInclude(bc => bc.Category)
-                .FirstOrDefaultAsync(m => m.ID == id);
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
 
-            if (book == null)
+            var category = await _context.Category.FirstOrDefaultAsync(m => m.ID == id);
+            if (category == null)
             {
                 return NotFound();
             }
             else
             {
-                Book = book;
+                Category = category;
             }
             return Page();
         }
