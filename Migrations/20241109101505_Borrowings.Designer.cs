5f74f6c05ea6d894ba33bb5de30720150fc28dc9
alexa <alexa@ALEXANDRA_NANU>
2024-11-09 13:00:20 +0200

    Laborator 5 primele 12 cerinte

--- Migrations/20241109101505_Borrowings.Designer.cs
+++ Migrations/20241109101505_Borrowings.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nanu_Alexandra_Lab2.Data;
 
@@ -11,9 +12,11 @@ using Nanu_Alexandra_Lab2.Data;
 namespace Nanu_Alexandra_Lab2.Migrations
 {
     [DbContext(typeof(Nanu_Alexandra_Lab2Context))]
-    partial class Nanu_Alexandra_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20241109101505_Borrowings")]
+    partial class Borrowings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Book.cs
+++ Models/Book.cs
@@ -21,7 +21,7 @@ namespace Nanu_Alexandra_Lab2.Models
 
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; }
-
+        public ICollection<Borrowing>? Borrowings { get; set; }
         public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
