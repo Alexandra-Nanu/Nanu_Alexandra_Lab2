d8b975ff5ebe3d485cb9a36501b49586dfaee6a2
alexa <alexa@ALEXANDRA_NANU>
2024-11-09 15:26:09 +0200

    Laborator 5 complet

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Nanu_Alexandra_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Nanu_Alexandra_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Nanu_Alexandra_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Nanu_Alexandra_Lab2Context' not found.")));
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Nanu_Alexandra_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Nanu_Alexandra_Lab2Context' not found."))); 
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
